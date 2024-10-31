82b3a9a79a5285e1dc57a598c7871d859ab4f7cb
NabilChabab <nabil.chababnabil@gmail.com>
2024-10-31 19:32:56 +0100

    search save

--- src/main/java/com/example/x_leagues/services/ParticipationService.java
+++ src/main/java/com/example/x_leagues/services/ParticipationService.java
@@ -2,8 +2,12 @@ package com.example.x_leagues.services;
 
 import com.example.x_leagues.model.Participation;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ParticipationService {
 
 
     Participation save(Participation participation);
+    Page<Participation> findAll(Pageable pageable);
 }
