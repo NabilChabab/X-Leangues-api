ac6980f099d538cb5c8a764733c7a862e543f269
NabilChabab <nabil.chababnabil@gmail.com>
2024-11-07 22:52:54 +0100

    competition results

--- src/main/java/com/example/x_leagues/services/CompetitionService.java
+++ src/main/java/com/example/x_leagues/services/CompetitionService.java
@@ -1,13 +1,18 @@
 package com.example.x_leagues.services;
 
 import com.example.x_leagues.model.Competition;
+import com.example.x_leagues.services.dto.CompetitionDetailsDTO;
 import org.springframework.data.domain.Page;
 
 
 import org.springframework.data.domain.Pageable;
 
+import java.util.UUID;
+
 public interface CompetitionService {
     Competition save(Competition competition);
 
     Page<Competition> findAll(Pageable pageable);
+
+    CompetitionDetailsDTO competitionDetails(UUID competitionId);
 }
