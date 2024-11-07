9335449878c703ea673a717c33f0b9003342a184
NabilChabab <nabil.chababnabil@gmail.com>
2024-11-07 22:52:39 +0100

    competition results

--- src/main/java/com/example/x_leagues/web/vm/mapper/CompetitionMapper.java
+++ src/main/java/com/example/x_leagues/web/vm/mapper/CompetitionMapper.java
@@ -2,6 +2,7 @@ package com.example.x_leagues.web.vm.mapper;
 
 
 import com.example.x_leagues.model.Competition;
+import com.example.x_leagues.services.dto.CompetitionDetailsDTO;
 import com.example.x_leagues.web.vm.competitions.CompetitionResponseVM;
 import com.example.x_leagues.web.vm.competitions.CompetitionVM;
 import org.mapstruct.Mapper;
@@ -11,4 +12,5 @@ public interface CompetitionMapper {
 
     Competition toEntity(CompetitionVM competitionVm);
     CompetitionResponseVM toVM(Competition competition);
+    CompetitionResponseVM toDetailsVM(CompetitionDetailsDTO competitionDetailsDTO);
 }
